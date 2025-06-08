813ce03380abe4d6ef56b81c15de4771ccce81b4
Oscar Stiven Ciro Alvarez <stivenciroalvarez@gmail.com>
2025-06-08 14:26:20 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -1,14 +1,18 @@
-import { initializeApp, getApps, getApp, FirebaseApp } from "firebase/app";
-import { getAuth, Auth } from "firebase/auth";
-import { getFirestore, Firestore } from "firebase/firestore";
+
+import { initializeApp, getApps, getApp, type FirebaseApp } from "firebase/app";
+import { getAuth, type Auth } from "firebase/auth";
+import { getFirestore, type Firestore } from "firebase/firestore";
+import { getAnalytics, type Analytics } from "firebase/analytics";
 
 const firebaseConfig = {
-  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  apiKey: "AIzaSyABoRY5kFqIDSkMv37TeJ5I7r0EBUzr8zU",
+  authDomain: "lexia-v1.firebaseapp.com",
+  databaseURL: "https://lexia-v1-default-rtdb.firebaseio.com",
+  projectId: "lexia-v1",
+  storageBucket: "lexia-v1.firebasestorage.app",
+  messagingSenderId: "1074238001164",
+  appId: "1:1074238001164:web:1def07eca272fb966dde29",
+  measurementId: "G-55RL568ELY"
 };
 
 let app: FirebaseApp;
@@ -21,4 +25,9 @@ if (!getApps().length) {
 const auth: Auth = getAuth(app);
 const db: Firestore = getFirestore(app);
 
-export { app, auth, db };
+let analytics: Analytics | undefined;
+if (typeof window !== 'undefined') {
+  analytics = getAnalytics(app);
+}
+
+export { app, auth, db, analytics };
